3a486ec8aae01cef9bfae1272e6364e59f86e162
robertlyc <merlinlyc@gmail.com>
2012-11-18 00:32:14 +0800

    add popover to ipad storyboard but still have bugs to fix

--- Psychologist/PsychologistViewController.m
+++ Psychologist/PsychologistViewController.m
@@ -16,10 +16,22 @@
 @implementation PsychologistViewController
 @synthesize diagnosis = _diagnosis;
 
+- (HappinessViewController *)splitViewHappinessViewController {
+    id hvc = [self.splitViewController.viewControllers lastObject];
+    if (![hvc isKindOfClass:[HappinessViewController class]]) {
+        hvc = nil;
+    }
+    return hvc;
+}
+
 - (void)setAndShowDiagnosis:(int)diagnosis {
     self.diagnosis = diagnosis;
     //segue
-    [self performSegueWithIdentifier:@"ShowDiagnosis" sender:self]; 
+    if ([self splitViewHappinessViewController]) {
+        [self splitViewHappinessViewController].happiness = diagnosis;
+    } else {
+        [self performSegueWithIdentifier:@"ShowDiagnosis" sender:self];
+    }
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
---
+++ Psychologist/SplitViewBarButtonItemPresenter.h
@@ -0,0 +1,13 @@
+//
+//  SplitViewBarButtonItemPresenter.h
+//  Psychologist
+//
+//  Created by RoBeRt on 12-11-17.
+//  Copyright (c) 2012年 RoBeRt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol SplitViewBarButtonItemPresenter <NSObject>
+@property (nonatomic, strong) UIBarButtonItem *splitViewBarButtonItem;
+@end
