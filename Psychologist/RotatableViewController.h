7aa2cd521f7245fde352973b42e0241cf10b4604
robertlyc <merlinlyc@gmail.com>
2012-11-17 23:31:07 +0800

    add Universal support

---
+++ Psychologist/RotatableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RotatableViewController.h
+//  Psychologist
+//
+//  Created by RoBeRt on 12-11-17.
+//  Copyright (c) 2012年 RoBeRt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RotatableViewController : UIViewController
+
+@end
---
+++ Psychologist/RotatableViewController.m
@@ -0,0 +1,21 @@
+//
+//  RotatableViewController.m
+//  Psychologist
+//
+//  Created by RoBeRt on 12-11-17.
+//  Copyright (c) 2012年 RoBeRt. All rights reserved.
+//
+
+#import "RotatableViewController.h"
+
+@interface RotatableViewController ()
+
+@end
+
+@implementation RotatableViewController
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation {
+    return YES;
+}
+
+@end
